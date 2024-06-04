65a7d6e7bc56d3b9b6724eb2b1eecd685d7e197f
Alexandra Rusak <alexandra.myagkova@gmail.com>
2024-06-04 18:24:15 +0300

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pr_test_react_redux_mui/',
+  base: 'https://alexandrarusak.github.io/pr_test_react_redux_mui/',
 })
