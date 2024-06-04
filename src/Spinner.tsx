30bf105a3d95497c353a01f32910b1af363a1353
Alexandra Rusak <alexandra.myagkova@gmail.com>
2024-06-04 17:57:23 +0300

    minor fixes

--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -1,10 +1,11 @@
 import {Box, CircularProgress} from "@mui/material";
 
-function Spinner () {
+function Spinner() {
 
-   return <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', m: 10 }}>
-        <CircularProgress />
-    </Box>
+    return (
+        <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', m: 10}}>
+            <CircularProgress/>
+        </Box>
+    )
 }
-
 export {Spinner}
